dccca6278a13568e93340912b4387f9055d3b6b5
Laura-BST12 <laura.andrea34@yahoo.com>
2022-07-21 12:55:38 -0500

    adding files to repo

---
+++ myStatements.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'cherry', 'lemon']
+my_dict = {'drink': 'coffee', 'milk': 'whole'}
+
+for item in my_list:
+    print(f'My favorite thing to eat:{item}')
+
+for _,value in my_dict.items():
+    print(f'My favorite thing to drink: {value}')
+
+#Bugs examples
+#my_dict=my_dict
+#my_list2=
